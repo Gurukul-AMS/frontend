99e09599a9449df9182f2f9c61a103ca77ffc8cc
uglyprincess <the.boy.who.lived.3x4@gmail.com>
2021-05-22 20:02:13 +0530

    Made Minor Changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,28 +7,69 @@ const useStyles = makeStyles({
   root: {
       display: 'flex',
       justifyContent: 'center',
-      backgroundSize: 'cover',
+      background: '#0cebeb',  /* fallback for old browsers */
+      background: '-webkit-linear-gradient(to right, #34e89e, #0f3443)',  /* Chrome 10-25, Safari 5.1-6 */
+      background: 'linear-gradient(to top, #34e89e, #1e5a73)', /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */      
       height:'100vh',
       maxWidth: '100%',
       overflow: 'hidden',
       objectFit: 'fill',
-      margin: 'auto auto 50px auto'
+      margin: 'auto auto 50px auto',
+      flexDirection: 'column',
+      textAlign: 'center'
 
   },
 
   button: {
 
-    backgroundColor: '#21094e',
+    backgroundColor: '#194350',
     color: 'white',
-    margin: '550px auto auto auto',
+    margin: '30% auto auto auto',
     padding: '25px 100px',
     transitionDuration: '0.5s',
     '&:hover' : {
 
-      backgroundColor: '#233e8b',
+      backgroundColor: '#1e6f5c',
       boxShadow: '5px 5px 5px grey'
     }
 
+  },
+
+  banner: {
+    margin: '50px auto',
+    width: '900px',
+    height: '800px',
+    backgroundSize: 'cover',
+    textAlign: 'center',
+    flex: '10',
+    backgroundColor: 'white',
+    borderRadius: '10%',
+    padding: '30px',
+    boxShadow: '20px 20px 1px #206a5d',
+    transitionDuration: '1s',
+    '&:hover': {
+      boxShadow: 'none',
+    }
+    
+  },
+  
+  heading: {
+    flex: '1',
+    margin: '50px auto 0 auto',
+    fontFamily: "'DM Sans', sans-serif", 
+    background: 'white',
+    width: '100%',
+    height: '120px'
+
+  },
+
+  h1 : {
+    fontSize: '70px',
+    margin: '20px auto',
+    transitionDuration: '0.5s',
+    '&:hover': {
+      textShadow: '5px 5px grey'
+    }
   }
 
 });
@@ -60,9 +101,19 @@ export default function Home(props) {
       }
     }
 
-    return (<div className={classes.root} style={{backgroundImage: 'url(background/home.jpg)'}}>
-      <div>
-          <Button className={classes.button} onClick={path} variant="contained">
+    function goHere(){
+
+      window.location = path;
+    }
+
+    return (<div className={classes.root} 
+        // style={{backgroundImage: 'url(background/home.jpg)'}}
+        >
+      <div className={classes.heading}>
+        <h1 className={classes.h1}>Welcome to Gurukul</h1>
+      </div>
+      <div className={classes.banner} style={{backgroundImage: 'url(background/undraw_Updated_resume_re_q1or.svg)'}}>
+          <Button className={classes.button} onClick={goHere} variant="contained">
               {value}
           </Button>
       </div>
